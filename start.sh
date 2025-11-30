c866743d994700a8978f6ceb9aeff243842d88d7
kebabkenraali <63752860+kebabkenraali@users.noreply.github.com>
2025-11-30 12:46:14 +0200

    Jos toimisi

--- start.sh
+++ start.sh
@@ -1,44 +1,230 @@
 #!/bin/bash
-# Odota käynnistystä
-sleep 10
-# Päivitä järjestelmä
-apt update -y
-apt install -y git python3-venv python3-pip psmisc # psmisc fuser:ia varten
-# Kloonaa ComfyUI, jos ei ole
-if [ ! -d "/workspace/ComfyUI" ]; then
-  cd /workspace
-  git clone https://github.com/comfyanonymous/ComfyUI
+
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+
+# Packages are installed after nodes so we can fix them...
+
+APT_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
+
+PIP_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
+
+NODES=(
+    "https://github.com/Gourieff/comfyui-reactor-node"
+    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
+    "https://github.com/Fannovel16/ComfyUI-Frame-Interpolation"
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/kijai/ComfyUI-WanVideoWrapper"
+)
+
+WORKFLOWS=(
+
+)
+
+CHECKPOINT_MODELS=(
+    "https://huggingface.co/Phr00t/WAN2.2-14B-Rapid-AllInOne/resolve/main/Mega-v3/wan2.2-rapid-mega-nsfw-aio-v3.1.safetensors"
+    "https://huggingface.co/Phr00t/WAN2.2-14B-Rapid-AllInOne/resolve/main/v10/wan2.2-i2v-rapid-aio-v10-nsfw.safetensors"
+    "https://huggingface.co/Wan-AI/Wan2.2-I2V-A14B/resolve/main/wan2.2_i2v_a14b.safetensors"
+)
+
+UNET_MODELS=(
+)
+
+LORA_MODELS=(
+    "https://huggingface.co/lopi999/Wan2.2-I2V_General-NSFW-LoRA/resolve/main/Wan2.2-I2V_General-NSFW-LoRA.safetensors"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+)
+
+ESRGAN_MODELS=(
+)
+
+CONTROLNET_MODELS=(
+)
+
+INSIGHTFACE_MODELS=(
+    "https://github.com/deepinsight/insightface/releases/download/1.0.0/inswapper_128.onnx"
+)
+
+FACEDETECTION_MODELS=(
+    "https://github.com/biubug6/Pytorch_Retinaface/releases/download/0.0.1/retinaface_resnet50.onnx"
+)
+
+FACERESTORE_MODELS=(
+    "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.8/GPEN-BFR-512.onnx"
+)
+
+FRAME_INTERPOLATION_MODELS=(
+    "https://github.com/hzwer/arXiv2023-RIFE/releases/download/v4.7/rife47.pth"
+)
+
+TEXT_ENCODERS=(
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/checkpoints" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/unet" \
+        "${UNET_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+    # Custom directories and models
+    mkdir -p "${COMFYUI_DIR}/models/insightface/models"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/insightface/models" \
+        "${INSIGHTFACE_MODELS[@]}"
+    mkdir -p "${COMFYUI_DIR}/models/facedetection"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/facedetection" \
+        "${FACEDETECTION_MODELS[@]}"
+    mkdir -p "${COMFYUI_DIR}/models/facerestore_models"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/facerestore_models" \
+        "${FACERESTORE_MODELS[@]}"
+    mkdir -p "${COMFYUI_DIR}/models/frame_interpolation"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/frame_interpolation" \
+        "${FRAME_INTERPOLATION_MODELS[@]}"
+    mkdir -p "${COMFYUI_DIR}/models/text_encoders"
+    provisioning_get_files \
+        "${COMFYUI_DIR}/models/text_encoders" \
+        "${TEXT_ENCODERS[@]}"
+    provisioning_print_end
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+            sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+            pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                   pip install --no-cache-dir -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip install --no-cache-dir -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    if [[ -z $2 ]]; then return 1; fi
+    
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    arr=("$@")
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Application will start now\n\n"
+}
+
+function provisioning_has_valid_hf_token() {
+    [[ -n "$HF_TOKEN" ]] || return 1
+    url="https://huggingface.co/api/whoami-v2"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $HF_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function provisioning_has_valid_civitai_token() {
+    [[ -n "$CIVITAI_TOKEN" ]] || return 1
+    url="https://civitai.com/api/v1/models?hidden=1&limit=1"
+
+    response=$(curl -o /dev/null -s -w "%{http_code}" -X GET "$url" \
+        -H "Authorization: Bearer $CIVITAI_TOKEN" \
+        -H "Content-Type: application/json")
+
+    # Check if the token is valid
+    if [ "$response" -eq 200 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    if [[ -n $HF_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?huggingface\.co(/|$|\?) ]]; then
+        auth_token="$HF_TOKEN"
+    elif 
+        [[ -n $CIVITAI_TOKEN && $1 =~ ^https://([a-zA-Z0-9_-]+\.)?civitai\.com(/|$|\?) ]]; then
+        auth_token="$CIVITAI_TOKEN"
+    fi
+    if [[ -n $auth_token ]];then
+        wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
+}
+
+# Allow user to disable provisioning if they started with a script they didn't want
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
 fi
-# Luo ja aktivoi venv
-cd /workspace/ComfyUI
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-fi
-source venv/bin/activate
-# Asenna riippuvuudet (CUDA 12.1)
-pip install --upgrade pip
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
-pip install -r requirements.txt
-# Asenna custom nodeja (pidetään toistaiseksi, mutta ilman malleja; jos ongelmia, kommentoi pois)
-mkdir -p custom_nodes # Varmista hakemisto
-cd custom_nodes
-if [ ! -d "ComfyUI-Manager" ]; then
-  git clone https://github.com/ltdrdata/ComfyUI-Manager
-fi
-if [ ! -d "ComfyUI-WanVideoWrapper" ]; then
-  git clone https://github.com/kijai/ComfyUI-WanVideoWrapper
-fi
-if [ ! -d "ComfyUI-VideoHelperSuite" ]; then
-  git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite
-fi
-if [ ! -d "ComfyUI-Frame-Interpolation" ]; then
-  git clone https://github.com/Fannovel16/ComfyUI-Frame-Interpolation
-fi
-if [ ! -d "comfyui-reactor-node" ]; then
-  git clone https://github.com/Gourieff/comfyui-reactor-node
-fi
-cd ..
-# Tapa portti, jos käytössä
-fuser -k 3000/tcp
-# Käynnistä ComfyUI
-python main.py --listen 0.0.0.0 --port 3000
